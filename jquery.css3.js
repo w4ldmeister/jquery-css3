72ba448d043791efc0bb2e7b3b80dfd32f52eca6
Christian Fischer <waldmeister23@googlemail.com>
2011-12-07 14:40:38 +0100

    initial

---
+++ jquery.css3.js
@@ -0,0 +1,54 @@
+(function( $ ){
+	var prefix = $.browser.webkit ? "-webkit-" : "-moz-";
+	var methods = {
+		gradient: function(opts) {
+			var options = $.extend({
+				base: "center right",
+				colors: {
+					"0"  : "FFFFFF",
+					"50" : "000000",
+					"100": "FFFFFF"
+				},
+				form: "linear"}
+				,opts);
+				console.log(options);
+			return this.each(function() {
+				var gradient="";
+				gradient += prefix+options.form+"-gradient";
+				gradient += "(";
+				gradient += options.base;
+
+				$.each(options.colors, function(hold,color) {
+					gradient += ",#"+color;
+					gradient += (" "+hold+"%" || "");
+				});
+				gradient += ")";
+				//console.log(gradient);        
+				$(this).css("background-image", gradient);
+
+			})
+		}, 
+		rotate : function( opts ) { 
+			var options = opts ? opts : "30deg";
+			//console.log(options);
+			return this.each(function(){
+				//console.log("rotate("+options+")");
+				$(this).css(prefix+"transform","rotate("+options+")");
+			})
+		}
+	};
+
+	$.fn.css3 = function( method ) {
+		
+		if ( methods[method] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments);
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+		}  
+
+
+
+	};
+})( jQuery );
